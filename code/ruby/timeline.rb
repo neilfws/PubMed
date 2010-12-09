57977b3063f102f1fc4f9ee2ef439778463c2862
Neil Saunders <neilfws@gmail.com>
2010-12-09 17:57:06 +1100

    Precalculate and store timeline; changes to views

---
+++ code/ruby/timeline.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+require "rubygems"
+require "mongo"
+
+db       = Mongo::Connection.new.db('pubmed')
+entries  = db.collection('entries')
+timeline = db.collection('timeline')
+
+dates = entries.find.map { |entry| entry['MedlineCitation']['DateCreated'] }
+dates.map! { |d| Date.parse("#{d['Year']}-#{d['Month']}-#{d['Day']}") }
+dates.sort!
+data = (dates.first..dates.last).inject(Hash.new(0)) { |h, date| h[date] = 0; h }
+dates.each { |date| data[date] += 1}
+data = data.sort
+data.map! {|e| ["Date.UTC(#{e[0].year},#{e[0].month - 1},#{e[0].day})", e[1]] }
+
+t = { "timeline" => data}
+timeline.save(t)
+
+puts "Saved #{timeline.find_one['timeline'].count} dates in timeline."
--- code/sinatra/main.rb
+++ code/sinatra/main.rb
@@ -2,19 +2,26 @@ configure do
   DB = Mongo::Connection.new.db('pubmed')
 end
 
-entries = DB.collection('entries')
-ecount  = DB.collection('ecount')
-$KCODE  = "u"
+entries  = DB.collection('entries')
+ecount   = DB.collection('ecount')
+timeline = DB.collection('timeline')
+$KCODE   = "u"
+
+# function to format records
+def pmformat(records)
+  "#{records.to_a.map {|r| r['MedlineCitation']}.inspect}"
+end
 
 # views
 get "/" do
-  @dates = entries.find.map { |entry| entry['MedlineCitation']['DateCreated'] }
-  @data  = @dates.inject(Hash.new(0)) { |h, date|
-    month   = date['Month'].to_i - 1
-    newdate = "Date.UTC(#{date['Year']}, #{month}, #{date['Day']})"
-    h[newdate] += 1
-    h
-  }
+  @data = timeline.find_one['timeline']
+  # @dates = entries.find().map { |entry| entry['MedlineCitation']['DateCreated'] }
+  # @dates.map! { |d| Date.parse("#{d['Year']}-#{d['Month']}-#{d['Day']}") }
+  # @dates.sort!
+  # @data = (@dates.first..@dates.last).inject(Hash.new(0)) { |h, date| h[date] = 0; h }
+  # @dates.each { |date| @data[date] += 1}
+  # @data = @data.sort
+  # @data.map! {|e| ["Date.UTC(#{e[0].year},#{e[0].month - 1},#{e[0].day})", e[1]] }
   haml :index
 end
 
@@ -37,6 +44,12 @@ get "/journals" do
   @journals = entries.find.inject(Hash.new(0)) {|h, e| h[e['MedlineCitation']['Article']['Journal']['ISOAbbreviation']] += 1; h }
   @journals = @journals.sort_by { |k,v| v}.reverse
   haml :journals
+end
+
+get "/date/*" do
+  @date = params[:splat].first.split("-")
+  @records = pmformat(entries.find({"MedlineCitation.DateCreated.Year" => @date[0], "MedlineCitation.DateCreated.Month" => @date[1], "MedlineCitation.DateCreated.Day" => @date[2]}))
+  haml :date
 end
 
 get "/testing" do
