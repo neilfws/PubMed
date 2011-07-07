0e0ed01ad60ec5ce06a0be16dc44a5e1d5a42d92
neilfws <neilfws@gmail.com>
2011-07-07 10:57:36 +1000

    mongo2dates.rb now gets PubDate year + month

--- retractions/npg/code/ruby/mongo2dates.rb
+++ retractions/npg/code/ruby/mongo2dates.rb
@@ -5,37 +5,51 @@ require "mongo"
 
 # db connection
 db     = Mongo::Connection.new.db('noorden')
-col    = db.collection('retracted')
+col1   = db.collection('retracted')
+col2   = db.collection('retractionOf')
 outdir = "#{ENV['HOME']}/Dropbox/projects/pubmed/retractions/npg/output"
 
-data = col.find.map do |record|
+data = col1.find.map do |record|
   [record['_id'],
-   record['MedlineCitation']['DateCreated'],
    record['MedlineCitation']['CommentsCorrectionsList']['CommentsCorrections'],
    record['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']
   ]
 end
 
 File.open("#{outdir}/retractions_dates.tsv", "w") do |f|
-  f.write "pmid\tyearCreated\tyearPublished\tpmidRetraction\tyearRetracted\tretractionType\n"
+  f.write "pmid\tyearPublished\tmonthPublished\tpmidRetraction\tyearRetracted\tmonthRetracted\tretractionType\n"
   # regexes in following code not specific for year (2 cases)
   data.each do |record|
     pmid1 = record[0]
-    year1 = record[1]['Year']
-    year3 = record[3]['Year']
-    if record[2].is_a? Hash
-      pmid2 = record[2]['PMID']
-      year2 = record[2]['RefSource'] =~ /\s+(19\d{2}|20\d{2})/ ? $1 : ""
-      type  = record[2]['RefType']
-    elsif record[2].is_a? Array
-      record[2].each do |ref|
+    year1 = record[2]['Year']
+    mon1  = record[2]['Month']
+    if record[1].is_a? Hash
+      type   = record[1]['RefType']
+      pmid2  = record[1]['PMID']
+      if col2.find("_id" => pmid2).count == 1
+        notice = col2.find("_id" => pmid2).first
+        year2  = notice['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Year']
+        mon2   = notice['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Month']
+      else
+        year2 = ""
+        mon2  = ""
+      end
+    elsif record[1].is_a? Array
+      record[1].each do |ref|
         if ref['RefType'] =~ /RetractionIn/
-          pmid2 = ref['PMID']
-          year2 = ref['RefSource'] =~ /\s+(19\d{2}|20\d{2})/ ? $1 : ""
-          type  = ref['RefType']
+          type   = ref['RefType']
+          pmid2  = ref['PMID']
+          if col2.find("_id" => pmid2).count == 1
+            notice = col2.find("_id" => pmid2).first
+            year2  = notice['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Year']
+            mon2   = notice['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Month']
+          else
+            year2 = ""
+            mon2  = ""
+          end
         end
       end
     end
-    f.write "#{pmid1}\t#{year1}\t#{year3}\t#{pmid2}\t#{year2}\t#{type}\n"
+    f.write "#{pmid1}\t#{year1}\t#{mon1}\t#{pmid2}\t#{year2}\t#{mon2}\t#{type}\n"
   end
 end
