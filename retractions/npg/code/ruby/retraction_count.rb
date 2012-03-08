9a8ddf8d8f92a6b517b37ebd0ddf5bd6e384344c
Neil Saunders <neilfws@gmail.com>
2012-03-09 09:13:56 +1100

    Use current year for final year instead of 2011

--- retractions/npg/code/ruby/retraction_count.rb
+++ retractions/npg/code/ruby/retraction_count.rb
@@ -2,6 +2,7 @@
 
 require "rubygems"
 require "bio"
+require "date"
 
 Bio::NCBI.default_email = "me@me.com"
 ncbi = Bio::NCBI::REST.new
@@ -9,7 +10,7 @@ outdir = "#{ENV['HOME']}/Dropbox/projects/pubmed/retractions/npg/output"
 
 File.open("#{outdir}/retractions.tsv", "w") do |f|
   f.write "year\tpublications\tretracted\tretractionOf\n"
-  1971.upto(2011) do |year|
+  1971.upto(Date.today.year) do |year|
     all           = ncbi.esearch_count("#{year}[dp]", {"db" => "pubmed"})
     retracted     = ncbi.esearch_count("Retracted Publication[ptyp] #{year}[dp]", {"db" => "pubmed"})
     retractionof  = ncbi.esearch_count("Retraction Of Publication[ptyp] #{year}[dp]", {"db" => "pubmed"})
