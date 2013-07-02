030bb28cc660d9289c2f4bc6838021912afadb9c
Neil Saunders <neilfws@gmail.com>
2013-07-02 11:21:55 +1000

    Refactored for any input text file

--- omics/code/R/omics.R
+++ omics/code/R/omics.R
@@ -2,18 +2,22 @@ library(ggplot2)
 
 setwd("~/Dropbox/projects/pubmed/omics")
 
-omics <- readLines("data/omics.txt")
-omics <- tolower(omics)
-
-omics.freq <- as.data.frame(table(omics))
-omics.freq <- omics.freq[ order(omics.freq$Freq, decreasing = T),]
-omics.freq$omics <- factor(omics.freq$omics, levels = omics.freq$omics)
-
+# words = output from:
+#  cut -f1 -d "," pubmed_result.csv | grep -ioP "(\w+omics)" > omics.txt
+getOmics <- function(words) {
+  omics <- readLines(words)
+  omics <- tolower(omics)
+  
+  omics.freq <- as.data.frame(table(omics))
+  omics.freq <- omics.freq[ order(omics.freq$Freq, decreasing = T),]
+  omics.freq$omics <- factor(omics.freq$omics, levels = omics.freq$omics)
 # plot top 20
-png(file = "output/omics20.png", width = 800, height = 600)
-print(ggplot(head(omics.freq, 20)) + geom_bar(aes(omics, Freq), stat = "identity", fill = "darkblue") +
-                                     theme_bw() + coord_flip())
-dev.off()
-
+  outfile <- paste("output/", basename(words), ".png", sep = "")
+  png(file = outfile, width = 800, height = 600)
+  print(ggplot(head(omics.freq, 20)) + geom_bar(aes(omics, Freq), stat = "identity", fill = "darkblue") +
+                                       theme_bw() + coord_flip())
+  dev.off()
 # write all
-write.table(omics.freq, "output/omics_freq.csv", quote = F, sep = ",", row.names = F)
+  outfile <- paste("output/", basename(words), ".csv", sep = "")
+  write.table(omics.freq, outfile, quote = F, sep = ",", row.names = F)
+}
