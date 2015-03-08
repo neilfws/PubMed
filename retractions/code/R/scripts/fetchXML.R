e0a63a76f5113cfa5aedc348d57ec024bbf5bc4d
Neil Saunders <neilfws@gmail.com>
2015-03-09 08:27:17 +1100

    Added ptyp argument to getXML(); allows for retracted or retraction of

--- retractions/code/R/scripts/fetchXML.R
+++ retractions/code/R/scripts/fetchXML.R
@@ -1,11 +1,11 @@
-getXML <- function() {
+getXML <- function(ptyp) {
   require(rentrez)
   require(XML)
-  es <- entrez_search("pubmed", "\"Retracted Publication\"[PTYP]", usehistory = "y")
+  es <- entrez_search("pubmed", paste(ptyp, "[PTYP]", sep = ""), usehistory = "y")
   ef <- entrez_fetch("pubmed", WebEnv = es$WebEnv, query_key = es$QueryKey, retmax = es$count, rettype = "xml")
   return(ef)
 }
 
 setwd("~/Dropbox/projects/pubmed/retractions/data/")
-retracted <- getXML()
+retracted <- getXML("\"Retracted Publication\"")
 writeLines(retracted, "retracted.xml")
