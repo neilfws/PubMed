afaadb0e90d215b18a2424dfa268d986a82d841c
Neil Saunders <neilfws@gmail.com>
2015-03-09 12:42:53 +1100

    Use PubDate/Year not Medline DateCreated

--- retractions/code/R/scripts/byyear.R
+++ retractions/code/R/scripts/byyear.R
@@ -1,16 +1,15 @@
 yearsToCSV <- function(xmlfile) {
   require(XML)
   require(rentrez)
-  require(lubridate)
   doc <- xmlTreeParse(xmlfile, useInternalNodes = TRUE)
-  dates <- xpathSApply(doc, "//MedlineCitation/DateCreated", xmlValue)
-  years <- year(ymd(dates))
-  ydf <- data.frame(year = (min(years) - 1):max(years), total = NA, retracted = NA)
-  for(y in (min(years) - 1):max(years)) {
+  dates <- xpathSApply(doc, "//MedlineCitation/Article/Journal/JournalIssue/PubDate/Year", xmlValue)
+  years <- as.numeric(dates)
+  ydf <- data.frame(year = min(years):max(years), total = NA, retracted = NA)
+  for(y in min(years):max(years)) {
     total <- entrez_search("pubmed", paste(y, "[DP]", sep = ""))
     retd  <- entrez_search("pubmed", paste("\"Retracted Publication\"[PTYP] ", y, "[DP]", sep = ""))
-    ydf[(y - (min(years) - 1) + 1), "total"]     <- total$count
-    ydf[(y - (min(years) - 1) + 1), "retracted"] <- retd$count
+    ydf[(y - min(years)) + 1, "total"]     <- total$count
+    ydf[(y - min(years)) + 1, "retracted"] <- retd$count
     Sys.sleep(2)
   }
   return(ydf)
