d5c35e94897aa30187b2a5b876e4c3e23bcbcc67
Neil Saunders <neilfws@gmail.com>
2015-10-02 08:56:01 +1000

    Edited fetchXML.R to reflect syntax changes in latest rentrez

--- retractions/code/R/scripts/fetchXML.R
+++ retractions/code/R/scripts/fetchXML.R
@@ -1,8 +1,9 @@
 getXML <- function(ptyp) {
   require(rentrez)
   require(XML)
-  es <- entrez_search("pubmed", paste(ptyp, "[PTYP]", sep = ""), usehistory = "y")
-  ef <- entrez_fetch("pubmed", WebEnv = es$WebEnv, query_key = es$QueryKey, retmax = es$count, rettype = "xml")
+  es <- entrez_search("pubmed", paste(ptyp, "[PTYP]", sep = ""), use_history = TRUE)
+#  ef <- entrez_fetch("pubmed", WebEnv = es$WebEnv, query_key = es$QueryKey, retmax = es$count, rettype = "xml")
+   ef <- entrez_fetch("pubmed", web_history = es$web_history, retmax = es$count, rettype = "xml")
   return(ef)
 }
 
