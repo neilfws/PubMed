f67b3a167fbf1f5f2bb3752202e167695d31dc59
Neil Saunders <neilfws@gmail.com>
2018-05-31 22:30:58 +1000

    Removed unused code

--- retractions/code/R/scripts/fetchXML.R
+++ retractions/code/R/scripts/fetchXML.R
@@ -1,9 +1,7 @@
 getXML <- function(ptyp) {
   require(rentrez)
-  require(XML)
   es <- entrez_search("pubmed", paste(ptyp, "[PTYP]", sep = ""), use_history = TRUE)
-#  ef <- entrez_fetch("pubmed", WebEnv = es$WebEnv, query_key = es$QueryKey, retmax = es$count, rettype = "xml")
-   ef <- entrez_fetch("pubmed", web_history = es$web_history, retmax = es$count, rettype = "xml")
+  ef <- entrez_fetch("pubmed", web_history = es$web_history, retmax = es$count, rettype = "xml")
   return(ef)
 }
 
