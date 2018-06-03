8935b41b23964ea6897bce57e7171ee2b1ed8cbe
Neil Saunders <neilfws@gmail.com>
2018-06-03 22:25:09 +1000

    Use years column in tibble, not vector. Code still very ugly.

--- retractions/code/R/scripts/byyear.R
+++ retractions/code/R/scripts/byyear.R
@@ -10,16 +10,17 @@ yearsToCSV <- function(xmlfile) {
     distinct() %>% 
     mutate(value = as.numeric(value)) %>% 
     expand(value = full_seq(value, 1)) %>% 
+    rename(years = value) %>% 
     mutate(total = NA)
   
-  for(y in min(years):max(years)) {
+  for(y in min(ydf$years):max(ydf$years)) {
     total <- entrez_search("pubmed", paste(y, "[CRDT]", sep = ""))
     Sys.sleep(3)
     cat(y, total$count, "\n")
-    ydf[(y - min(years)) + 1, "total"]     <- total$count
+    ydf[(y - min(ydf$years)) + 1, "total"]     <- total$count
   }
   return(ydf)
 }
 
 years <- yearsToCSV("~/Dropbox/projects/github_projects/pubmed/retractions/data/retracted.xml")
-write_csv(years, file = "~/Dropbox/projects/github_projects/PubMed/retractions/data/years.csv")
+write_csv(years, "~/Dropbox/projects/github_projects/PubMed/retractions/data/years.csv")
