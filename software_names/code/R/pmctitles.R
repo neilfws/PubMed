3e6476e76760977bad8bb907f86363feb0b4385b
Neil Saunders <neilfws@gmail.com>
2019-05-12 21:15:05 +1000

    Rewrite for tidyverse fixes missing rows bug; new pmctitles_matched file

--- software_names/code/R/pmctitles.R
+++ software_names/code/R/pmctitles.R
@@ -1,16 +1,18 @@
+library(readr)
+library(dplyr)
+
 setwd("~/Dropbox/projects/pubmed/software_names/")
-ti <- read.delim("data/pmctitles.tsv", header=FALSE, stringsAsFactors=FALSE)
-colnames(ti)    <- c("uid", "pretitle", "title")
-ti$pretitle.low <- tolower(ti$pretitle)
 
-ti.uniq <- ti[!duplicated(ti[, "uid"]), ]
-ti.cnt  <- as.data.frame(table(ti.uniq$pretitle.low), stringsAsFactors = FALSE)
-ti.cnt  <- subset(ti.cnt, Freq > 1)
-ti.cnt  <- ti.cnt[order(ti.cnt$Freq, decreasing = TRUE), ]
-
-ti.one  <- ti.cnt[-grep(" ", ti.cnt$Var1), ]
-ti.one10 <- subset(ti.one, Freq < 11)
-table(ti.one10$Freq)
-
-ti.in <- ti.in[order(ti.in$pretitle.low), ]
-write.table(ti.in, file = "output/pmctitles_matched.tsv", sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+read_delim("data/pmctitles.tsv", 
+           "\t",
+           escape_double = FALSE,
+           col_names = FALSE, 
+           trim_ws = TRUE) %>% 
+  rename(uid = X1, pretitle = X2, title = X3) %>% 
+  mutate(pretitle_low = tolower(pretitle)) %>% 
+  distinct(uid, .keep_all = TRUE) %>% 
+  group_by(pretitle_low) %>% 
+  filter(between(n(), 2, 10), !grepl(" ", pretitle_low)) %>% 
+  ungroup() %>% 
+  arrange(pretitle_low) %>% 
+  write_tsv("output/pmctitles_matched.tsv")
